a177f0874d5c75c5d783f9ac4916069e158f845b
Victor <victorhugo.anizau@outlook.com>
2026-01-22 01:44:51 -0300

    refactor(task-page):estiliza e reposiciona elementos na tela de detalhes

--- src/pages/TaskPages.jsx
+++ src/pages/TaskPages.jsx
@@ -7,8 +7,8 @@ function TaskPage() {
   const title = searchParams.get("title");
   const description = searchParams.get("description");
   return (
-    <div className="h-screen w-screen p-6 bg-slate-500">
-      <div className="w-125 flex flex-col space-y-4">
+    <div className=" flex justify-center h-screen w-screen p-6 bg-slate-500">
+      <div className="w-screen flex flex-col space-y-4">
         <div className="flex justify-center relative mb-6">
           <button
             className="absolute left-0 top-0 bottom-0 text-slate-100"
